cf94c5a6cfcad0ee040bafda50ab889698765cb7
Aparna-015 <Aparnam@1995>
2025-03-06 14:56:51 +0530

    ui updation

---
+++ src/MainRoute.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import AdminDashboard from './Pages/AdminDashboard/AdminDashboard';
+import Register from './Pages/auth/Components/Register';
+import Login from './Pages/auth/Components/Login';
+import LoginRegisterPage from './Pages/auth/LoginRegisterPage';
+import AdminPackageDetails from './Pages/PackageDetails/PackageDetails';
+import Navbar from './Components/navbar/Navbar';
+
+const isAuthenticated = () => {
+    console.log(document,"cookie")
+    const token = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('token='));
+    return !!token;
+  };
+
+  const PrivateRoute = ({ element: Component }) => {
+    return isAuthenticated() ? <Component /> : <Navigate to="/admin/login" replace />;
+  };
+  
+
+const MainRoute = () => {
+   
+  return (
+    
+    <>
+   <BrowserRouter>
+    {/* Render the Header component only for authenticated pages */}
+    {isAuthenticated() && <Navbar/>}
+
+    <Routes>
+      <Route path="/" element={<PrivateRoute element={AdminDashboard} />} />
+      <Route path="/package_details/:package_id" element={<PrivateRoute element={AdminPackageDetails} />} />
+      <Route path="/login_register" element={<LoginRegisterPage/>} />
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default MainRoute
